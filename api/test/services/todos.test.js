d309825c0fa7cae0367d8f3825e1ce3a3304a3f5
iva2k <iva2k@yahoo.com>
2018-06-28 12:46:28 -0700

    Return Promise from tests.

--- api/test/services/todos.test.js
+++ api/test/services/todos.test.js
@@ -9,7 +9,7 @@ describe('\'todos\' service', function() {
     done();
   });
 
-  it('creates and processes todo, adds user information', async function() {
+  it('creates and processes todo, adds user information', function() {
     // Create a new user we can use for testing
     //TODO: const user = await app.service('users').create({
     //  email: 'messagetest@example.com',
@@ -19,14 +19,16 @@ describe('\'todos\' service', function() {
 
     // The messages service call params (with the user we just created)
     const params = { user };
-    const todo = await app.service('todos').create({
+
+    return app.service('todos').create({
       title: 'a test',
       additional: 'should be removed'
-    }, params);
-
-    assert.equal(todo.title, 'a test');
-    //TODO: assert.equal(todo.userId, user._id); // `userId` should be set to the passed user
-    assert.ok(!todo.additional); // Additional property has been removed
-    //TODO: assert.deepEqual(todo.user, user); // `user` has been populated
+    }, params)
+      .then(todo => {
+        assert.equal(todo.title, 'a test');
+        //TODO: assert.equal(todo.userId, user._id); // `userId` should be set to the passed user
+        assert.ok(!todo.additional); // Additional property has been removed
+        //TODO: assert.deepEqual(todo.user, user); // `user` has been populated
+      });
   });
 });
